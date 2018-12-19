5b2478b4348f58a11aaf1db56bd5462ba3042c9b
Ivan Fraixedes <ivan@fraixed.es>
2018-12-19 19:10:48 +0100

    metadata: Define mDatas type & impl Format method
    Define the type which is a slice of MD and implement its Format method
    with its test.

--- metadata_test.go
+++ metadata_test.go
@@ -57,3 +57,51 @@ func TestMD_Format(t *testing.T) {
 		})
 	}
 }
+
+func TestMDatas_Format(t *testing.T) {
+	var tcases = []struct {
+		desc   string
+		format string
+		mds    mDatas
+		expout string
+	}{
+		{
+			desc:   "'v' verb",
+			format: "%v",
+			mds:    mDatas{{K: "first-key", V: "first-value"}, {K: "second-key", V: "second-value"}},
+			expout: "[{\"first-key\": first-value},{\"second-key\": second-value}]",
+		},
+		{
+			desc:   "'v' verb when value is a mix of types",
+			format: "%v",
+			mds: mDatas{
+				{K: "struct-key", V: struct{ Name string }{Name: "Ivan"}},
+				{K: "string-key", V: "string value"},
+				{K: "num-key", V: 53.473},
+			},
+			expout: "[{\"struct-key\": {Name:Ivan}},{\"string-key\": string value},{\"num-key\": 53.473}]",
+		},
+		{
+			desc: "any other verb",
+			format: func() string {
+				var verbs = [...]string{
+					"t", "b", "c", "d", "o", "q", "x", "X", "U", "e", "E", "f", "F", "g", "G", "q", "p", "s",
+				}
+
+				return fmt.Sprintf("%%%s", verbs[rand.Intn(len(verbs))])
+			}(),
+			mds:    mDatas{{K: "some-key", V: "some-value"}},
+			expout: "",
+		},
+	}
+
+	for i := range tcases {
+		var tc = tcases[i]
+		t.Run(tc.desc, func(t *testing.T) {
+			t.Parallel()
+
+			var out = fmt.Sprintf(tc.format, tc.mds)
+			assert.Equal(t, tc.expout, out)
+		})
+	}
+}
